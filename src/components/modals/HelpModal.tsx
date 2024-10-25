ab9d424187d494d0c73d3fc6a3a242cd0946bb4a
prapooskur <61389919+prapooskur@users.noreply.github.com>
2024-10-25 14:25:15 -0700

    ui changes for help modal

--- src/components/modals/HelpModal.tsx
+++ src/components/modals/HelpModal.tsx
@@ -12,18 +12,24 @@ const HelpModal: React.FC<Props> = ({opened, onClose}) => (
       <Modal
         opened={opened}
         onClose={onClose}
-        title="Help"
+        // title="Help"
+        size="lg"
+        // centered
+        withCloseButton={false}
         overlayProps={{
           backgroundOpacity: 0.55,
           blur: 3,
         }}
       >
-        <p>1. Double-click the left mouse button to create a node.</p>
-        <p>2. Select nodes by left clicking them once.</p>
-        <p>3. Delete nodes and edges by selecting them and pressing the delete key.</p>
-        <p>4. Connect nodes by clicking on an open edge and dragging it to another node.</p>
-        <p>5. Change the title and shape of a node by right clicking it.</p>
-        <p>6. Use the controls on the bottom left for zooming, recentering, and locking.</p>
+        <h2 style={{ textAlign: 'center' }}>Help</h2>
+        <ul>
+            <li>Double-click the left mouse button to create a node.</li>
+            <li>Select nodes by left-clicking them once.</li>
+            <li>Delete nodes and edges by selecting them and pressing the delete key.</li>
+            <li>Connect nodes by clicking on an open edge and dragging it to another node.</li>
+            <li>Change the title and shape of a node by right-clicking it.</li>
+            <li>Use the controls on the bottom left for zooming, recentering, and locking.</li>
+        </ul>
         <Button 
             variant="filled" 
             color="teal" 
@@ -31,7 +37,7 @@ const HelpModal: React.FC<Props> = ({opened, onClose}) => (
             style={{
                 left: '50%',  // Center the button horizontally
                 transform: 'translateX(-50%)',  // Offset by half of its width to align in center
-                // padding: '10px 20px',
+                padding: '10px 20px',
                 fontSize: '16px',
                 // border: '2px solid #2c2c2c',
             }}
