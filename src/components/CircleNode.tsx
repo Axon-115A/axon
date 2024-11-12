8b4e02ea1474a33fef905fb9a0cce9a2c9a5086a
prapooskur <61389919+prapooskur@users.noreply.github.com>
2024-11-12 12:43:30 -0800

    fixed bug where edges would disappear on shape change

--- src/components/CircleNode.tsx
+++ src/components/CircleNode.tsx
@@ -12,21 +12,11 @@ const CircleNode = ({ data }: any) => {
 			}}>
 				{data.label}
 			</div>
-			{/* superimposing a big transparent handle on top of the existing one to create our desired effect */}
-			<Handle type="source" position={Position.Top} className="invisibleHandle" id="topFake" />   
-			{/* <Handle type="source" position={Position.Top} className="handle" id="top" />  */}
 			
-			<Handle type="source" position={Position.Left} className="invisibleHandle" id="leftFake" />
-			{/* <Handle type="source" position={Position.Left} className="handle" id="left" />  */}
-
-
-			<Handle type="source" position={Position.Right} className="invisibleHandle" id="rightFake" />
-			{/* <Handle type="source" position={Position.Right} className="handle" id="right" />  */}
-
-			<Handle type="source" position={Position.Bottom} className="invisibleHandle" id="bottomFake" />
-			{/* <Handle type="source" position={Position.Bottom} className="handle" id="bottom" />  */}
-
-
+			<Handle type="source" position={Position.Top} id="top" /> 
+			<Handle type="source" position={Position.Left}  id="left" /> 
+			<Handle type="source" position={Position.Right} id="right" /> 
+			<Handle type="source" position={Position.Bottom}  id="bottom" /> 
 		</div>
 	);
 };
