97a09cc28ef5f298e88e1ea99c8a5ea034238edd
prapooskur <61389919+prapooskur@users.noreply.github.com>
2024-11-15 14:59:19 -0800

    added defaults

--- src/components/RectNode.tsx
+++ src/components/RectNode.tsx
@@ -24,7 +24,7 @@ const RectNode = ({ data }: any) => {
 
     return (
         <div className="rect" style={{ background: data.backgroundColor }}>
-            <div style={{ color: luminanceFromRGB(data.backgroundColor) < 128 ? 'white' : 'black' }}>
+            <div style={{ color: luminanceFromRGB(data.backgroundColor ?? "#6c5ce7") < 128 ? 'white' : 'black' }}>
                 {data.label}
             </div>
             <Handle type="source" position={Position.Top} id="top" />
