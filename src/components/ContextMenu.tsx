790034f31f253978891dc498fc4c700ce3ef4213
nmakranyi <nmakrany@ucsc.edu>
2024-11-17 16:31:08 -0800

    added icons to the node toolbar and changed appearance

--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -2,6 +2,12 @@ import { ControlledMenu, MenuItem } from '@szhsin/react-menu';
 import '@szhsin/react-menu/dist/index.css';
 import '@szhsin/react-menu/dist/theme-dark.css';
 import { FC } from 'react';
+import Palette from './../assets/palette.svg';
+import ColorWheel from './../assets/colorWheel.svg';
+import Shapes from './../assets/shapes.svg';
+import Rename from './../assets/pencil-square.svg';
+import TrashIcon from './../assets/trash3.svg';
+import './styles/ContextMenu.css'
 
 interface ContextMenuProps {
     isOpen: boolean;
@@ -18,24 +24,35 @@ interface ContextMenuProps {
 const ContextMenu: FC<ContextMenuProps> = ({ isOpen, setOpen, anchorX, anchorY, onShapeChange, onEdit, onDelete, onColorChange }) => {
 	return (
         <ControlledMenu
-            anchorPoint={{x: anchorX, y: anchorY}}
+            anchorPoint={{x: anchorX - 75, y: anchorY}}
             state={isOpen ? 'open' : 'closed'}
-            direction="right"
+            direction="top"
             onClose={() => setOpen(false)}
             theming='dark'
+            className="custom-menu"
         >
+            <div className="menu-row">
             <MenuItem value="Change Color" onClick={onColorChange}>
-                Change Color
-            </MenuItem>
-            <MenuItem value="Change Shape" onClick={onShapeChange}>
-                Change Shape
-            </MenuItem>
-            <MenuItem value="Rename" onClick={onEdit}>
-                Rename
-            </MenuItem>
-            <MenuItem value="Delete" onClick={onDelete}>
-                Delete
-            </MenuItem>
+                    <div>
+                        <img src={ColorWheel} />
+                    </div>
+                </MenuItem>
+                <MenuItem value="Change Shape" onClick={onShapeChange}>
+                    <div>
+                        <img src={Shapes} />
+                    </div>
+                </MenuItem>
+                <MenuItem value="Rename" onClick={onEdit}>
+                    <div>
+                        <img src={Rename} />
+                    </div>
+                </MenuItem>
+                <MenuItem value="Delete" onClick={onDelete}>
+                    <div>
+                        <img src={TrashIcon} />
+                    </div>
+                </MenuItem>
+            </div>
         </ControlledMenu>
       );
 };
